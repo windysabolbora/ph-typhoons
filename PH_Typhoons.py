e7eccdc20feb87ec6637968ce472ab7817619bff
Windy Sabolbora <windy.sabolbora@wvsu.edu.ph>
2024-04-28 12:39:22 +0800

    update

--- PH_Typhoons.py
+++ PH_Typhoons.py
@@ -16,7 +16,7 @@ if "model" not in st.session_state:
 def app():
     st.subheader('RNN-LSTM Based Typhoon Prediction in the Philippines')
     
-    text = """Prof. Louie F. Cervantes, M. Eng. (Information Engineering)
+    text = """Windy C. Sabolbora BSCS 3B 
     \nCCS 229 - Intelligent Systems
     *Department of Computer Science
     *College of Information and Communications Technology
